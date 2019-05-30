53b0df37e9c9ae9b2ee6de360c97762016efeb7c
Ray <ray20020809@gmail.com>
2019-05-31 00:40:20 +0800

    fix sth

--- test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-echo preg_replace('/(<meta property=\"og:title" content=\").{1,}(\">)/', '<meta property="og:title" content="' . $heading_title . '">', '233333<meta property="og:title" content="Laine S. 文房具">');
-
-?>
